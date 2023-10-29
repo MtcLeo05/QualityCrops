ce14f18b0291e711d8dc117c20a66a5585496914
MtcLeo05 <103683964+MtcLeo05@users.noreply.github.com>
2023-10-29 17:19:40 +0100

    1° Release, 3° Major, 1° Minor. Item Tags revamp Fertilizer stacks to 16 and is consumed Mod compatible with Variable Mob Height

--- src/main/java/com/mtcleo05/qualitycrops/utils/Utils.java
+++ src/main/java/com/mtcleo05/qualitycrops/utils/Utils.java
@@ -2,6 +2,7 @@ package com.mtcleo05.qualitycrops.utils;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.Objects;
